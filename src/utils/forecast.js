bfe1816fd1b4a1a2e268036d6b98c2e47981e516
pradeep nayak <nayakpradeep47@gmail.com>
2021-11-08 11:17:59 +0530

    humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -19,7 +19,7 @@ const forecast = (latitude, longitude, callback) => {
         body.current.weather_descriptions[0] +
           ". It is currently " +
           body.current.temperature +
-          " degress out."
+          " degress out. The humidity is " + body.current.humidity 
       );
     }
   });
